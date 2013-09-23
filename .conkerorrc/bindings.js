4a2def7baa630fd50f5e2c27ff75b7c0cf389ee6
han semaj <sangho.nah@gmail.com>
2013-09-23 18:17:32 +1200

    scrollTop and web jumps

---
+++ .conkerorrc/bindings.js
@@ -0,0 +1,8 @@
+define_key(content_buffer_normal_keymap, "M-<", "cmd_scrollTop")
+define_webjump("reddit", "http://reddit.com");
+define_webjump("github", "https://github.com");
+define_webjump("project euler", "http://projecteuler.net");
+define_webjump("gmail", "https://mail.google.com");
+define_webjump("arch wiki", "http://wiki.archlinux.org");
+define_webjump("youtube", "http://youtube.com");
+define_webjump("infoq", "http://infoq.com");
