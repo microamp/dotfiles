a8d7c28238fc3dba32b70a7838719edb8b28ce82
han semaj <sangho.nah@gmail.com>
2013-12-06 16:22:32 +1300

    additional web jumps

--- .conkerorrc/bindings.js
+++ .conkerorrc/bindings.js
@@ -6,3 +6,6 @@ define_webjump("gmail", "https://mail.google.com");
 define_webjump("arch wiki", "http://wiki.archlinux.org");
 define_webjump("youtube", "http://youtube.com");
 define_webjump("infoq", "http://infoq.com");
+define_webjump("twitter", "https://twitter.com");
+define_webjump("steam", "https://store.steampowered.com");
+define_webjump("ddg", "https://duckduckgo.com");
