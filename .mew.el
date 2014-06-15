8a7d38b8b2d2228331ad212d018d294896f6ccf9
han semaj <sangho.nah@gmail.com>
2014-06-15 23:24:41 +1200

    summary form customised

--- .mew.el
+++ .mew.el
@@ -15,6 +15,30 @@
 (setq mew-smtp-auth t)
 (setq mew-smtp-ssl t)
 
+(setq mew-use-unread-mark t)
+
+(setq mew-biff-interval 1)
+(setq mew-use-biff t)
+(setq mew-use-biff-bell t)
+(setq mew-passwd-timer-unit 60)
+(setq mew-passwd-lifetime 8)
+
+(setq mew-summary-form '(type " [" (4 year) "-" (5 date) " " (5 time) "] | " (30 from) " | " t (0 subj)))
+(setq mew-summary-form-extract-rule '(address))
+
+(defun mew-summary-form-date ()
+  "A function to return a date, MM-DD."
+  (let ((s (MEW-DATE)))
+    (when (or (string= s "")
+              (not (string-match mew-time-rfc-regex s)))
+      (setq s (mew-time-ctz-to-rfc
+               (mew-file-get-time (mew-expand-msg (MEW-FLD) (MEW-NUM))))))
+    (if (string-match mew-time-rfc-regex s)
+      (format "%02d-%02d"
+              (mew-time-mon-str-to-int (mew-time-rfc-mon))
+              (mew-time-rfc-day))
+      "")))
+
 (setq mew-ssl-verify-level 0)
 
 (setq mew-use-cached-passwd t)
