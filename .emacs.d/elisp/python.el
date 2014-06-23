df9c93ab25cbee6075a31fe507f3d36ff3872303
han semaj <sangho.nah@gmail.com>
2014-06-24 09:22:23 +1200

    ipython settings added

--- .emacs.d/elisp/python.el
+++ .emacs.d/elisp/python.el
@@ -4,23 +4,32 @@
 (eval-after-load "flymake" '(require 'flymake-python-pyflakes))
 (require 'jedi)
 
-;; fill-column-indicator
+;; turn on smart indentation
+(setq py-smart-indentation t)
+
+;; use IPython as default shell
+(setq-default py-shell-name "ipython")
+
+;; switch to interpreter after executing code
+(setq py-shell-switch-buffers-on-execute-p t)
+(setq py-switch-buffers-on-execute-p t)
+
+;; fill column indicator
 (setq-default fci-rule-column 79)
 (setq-default fci-rule-color "yellow")
 
-;; flymake-python-pyflakes
+;; flake8 linting
 (setq flymake-python-pyflakes-executable "flake8")
 
-;; jedi (auto-completion)
+;; jedi (for auto-completion)
 (setq jedi:setup-keys t)
 (setq jedi:complete-on-dot t)
-;(jedi:install-server)
+(jedi:install-server)
 
 ;; python hooks
 (add-hook 'python-mode-hook 'fci-mode)
 (add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
 (add-hook 'python-mode-hook 'jedi:setup)
 (add-hook 'python-mode-hook 'linum-mode)
-(add-hook 'python-mode-hook (lambda nil (jedi:install-server)))
 
 (provide 'python)
