7cceb61b0b693396773a0431e2492909abf44966
james sangho nah <sangho.nah@gmail.com>
2017-04-01 16:11:39 +1300

    Git ignore for macOS

--- install.sh
+++ install.sh
@@ -13,3 +13,11 @@ echo "symlinked: ${FILE}"
 FILE=".vimrc"
 ln -s $CWD/$FILE $HOME/$FILE
 echo "symlinked: ${FILE}"
+
+FILE=".macos_gitignore"
+ln -s $CWD/$FILE $HOME/$FILE
+echo "symlinked: ${FILE}"
+
+git config --global core.excludesfile $HOME/$FILE
+
+echo "[Git] Globally ignored: ${HOME}/${FILE}"
